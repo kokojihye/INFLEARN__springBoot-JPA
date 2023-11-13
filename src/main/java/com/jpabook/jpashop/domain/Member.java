00d8a8436293cb366ff8b2f182761d27e8ee913d
kokojihye <jihyevivianako@gmail.com>
2023-11-13 16:49:48 +0900

    section2: develop entity class

---
+++ src/main/java/com/jpabook/jpashop/domain/Member.java
@@ -0,0 +1,25 @@
+package com.jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+public class Member {
+
+    @Id @GeneratedValue
+    @Column(name ="member_id")
+    private Long id;
+
+    private String name;
+
+    @Embedded
+    private Address address;
+
+    @OneToMany(mappedBy = "member") //Order클래스의 @ManyToOne 달린 member(변수명)
+    private List<Order> orders = new ArrayList<>();
+}
